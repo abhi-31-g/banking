a8cacfa88faf0ff4c3411e16a5b8a9b95bee4997
abhi-31-g <abhinav2002.2002@gmail.com>
2020-05-02 10:15:01 +0530

    don't know what changes are being accomodated

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
 import os
+from jinja2 import Template
 
 app=Flask("Application")
 
+
 @app.route("/")
 def index():
-    return render_template("welcome.html")
+  return render_template("welcome.html")
+
+def bank(x):
+    global var
+    if x==1:
+        var="banker"
+    elif x==2:
+        var="player"
+    else :
+        var="banpla"
+    return 
