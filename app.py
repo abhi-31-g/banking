0bc1a971499756ee1fdfe56d0032c920e4cca231
abhi-31-g <abhinav2002.2002@gmail.com>
2020-05-04 17:08:36 +0530

    NEW FILES ADDED FOR BANKER.HTML

--- app.py
+++ app.py
@@ -15,17 +15,19 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
   return render_template("welcome.html")
 
-@app.route("/banker?=login/",method=["POST"])
+@app.route("/banker?=login/",methods=["POST"])
 def createtable():
   username=request.form.get("Username")
   players=int(request.form.get("players"))
   password="#"+str(random.randint(1000,9999))+"$"
   sessioncode=''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
   try:
-    db.execute("create table :scode ( id PRIMARY KEY SERIAL NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);",{"scode"= sessioncode})
+    db.execute("create table :scode ( id PRIMARY KEY SERIAL NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);",{"scode": sessioncode})
     db.commit
   except:
     createtable()
+  return render_template("banker.html",sesscode=sessioncode,passw=password)
+  
   
   
   
