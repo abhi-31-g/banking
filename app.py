0c8b0d81fc8c57bdfec09e409f9010397e14fb56
abhi-31-g <abhinav2002.2002@gmail.com>
2020-05-04 13:59:30 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, request
 import os
 from jinja2 import Template
+import sqlalchemy as sql
+import random
+import string
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app=Flask("Application")
+engine=sql.create_engine('postgresql://postgres:veronica31@localhost:5432/bank')
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
   return render_template("welcome.html")
 
-def bank(x):
-    global var
-    if x==1:
-        var="banker"
-    elif x==2:
-        var="player"
-    else :
-        var="banpla"
-    return 
+@app.route("/banker?=login/",method=["POST"])
+def createtable():
+  username=request.form.get("Username")
+  players=int(request.form.get("players"))
+  password="#"+str(random.randint(1000,9999))+"$"
+  sessioncode=''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+  try:
+    db.execute("create table :scode ( id PRIMARY KEY SERIAL NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);",{"scode"= sessioncode})
+    db.commit
+  except:
+    createtable()
+  
+  
+  
