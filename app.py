026e606f8876e5fab04d17a9baaabcd97726db4f
abhi-31-g <abhinav2002.2002@gmail.com>
2020-05-07 19:27:44 +0530

    the banker is succesfully creating a session

--- app.py
+++ app.py
@@ -19,15 +19,11 @@ def index():
 def createtable():
   username=request.form.get("Username")
   players=int(request.form.get("players"))
-  password="#"+str(random.randint(1000,9999))+"$"
-  sessioncode=''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+  password=str(random.randint(10000,99999))
+  sessioncode=''.join(random.choices(string.ascii_uppercase, k = 8))
   try:
-    db.execute("create table :scode ( id PRIMARY KEY SERIAL NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);",{"scode": sessioncode})
+    db.execute("create table "+ sessioncode +" ( id SERIAL PRIMARY KEY NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);")
     db.commit
   except:
     createtable()
   return render_template("banker.html",sesscode=sessioncode,passw=password)
-  
-  
-  
-  
