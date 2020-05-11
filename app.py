4157f9aaffd42ff5ae49c3aba6177c4016856c76
abhi-31-g <abhinav2002.2002@gmail.com>
2020-05-11 16:14:13 +0530

    Tables are also created in the database and worked for the button to redirect to bankers page

--- app.py
+++ app.py
@@ -21,9 +21,25 @@ def createtable():
   players=int(request.form.get("players"))
   password=str(random.randint(10000,99999))
   sessioncode=''.join(random.choices(string.ascii_uppercase, k = 8))
+  passwi='#'+password+'$'
   try:
     db.execute("create table "+ sessioncode +" ( id SERIAL PRIMARY KEY NOT NULL, Name VARCHAR NOT NULL, Balance NUMERIC, Liabilities NUMERIC, Assets VARCHAR);")
-    db.commit
+    db.commit()
+    db.execute("insert into sessionslog (sesscode, password) values (:scode,:passwi);",{'scode': sessioncode,'passwi': passwi})
+    db.commit()
   except:
     createtable()
+  db.close()
   return render_template("banker.html",sesscode=sessioncode,passw=password)
+
+@app.route("/player?=login/",methods=["POST"])
+def login():
+  scode=str(request.form.get("scode"))
+  passwin=str(request.form.get("passwd"))
+  scode.upper()
+  passwin.strip('')
+  passwic="#"+passwin+"$"
+  if db.execute("select * from sessionslog where sesscode= :scode and password= :passwic ;",{"scode": scode,"passwic": passwic}).rowcount==1:
+    return "WELCOME TO THE SESSION"
+  else:
+    return "UNKNOWN ERROR"
